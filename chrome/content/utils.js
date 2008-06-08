40f5932475280fe2ad6dd53a8bd383de3e388639
Roman Timushev <rtimush@gmail.com>
2008-06-08 09:56:44 +0000

    Добавлена возможность устанавливать фоновое изображение.
    git-svn-id: http://svn.assembla.com/svn/firefox-desktop/trunk@5 fbf1ad31-3caa-4afa-b5b4-094426959cba

--- chrome/content/utils.js
+++ chrome/content/utils.js
@@ -79,5 +79,10 @@ var Utils = {
     var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
              .getService(Components.interfaces.nsIWindowMediator);
     return wm.getMostRecentWindow("navigator:browser");
+  },
+  
+  getBrowser: function() {
+    return Utils.getBrowserWindow().getBrowser();
   }
+  
 }

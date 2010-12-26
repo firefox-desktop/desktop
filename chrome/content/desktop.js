bbe5a71048cb552f48c87abd5c4e417897e0eef0
Roman Timushev <rtimush@gmail.com>
2010-12-26 16:26:46 +0000

    Ability to stretch/tile background
    git-svn-id: http://svn.assembla.com/svn/firefox-desktop/trunk@49 fbf1ad31-3caa-4afa-b5b4-094426959cba

--- chrome/content/desktop.js
+++ chrome/content/desktop.js
@@ -1,38 +1,38 @@
 Desktop = {
 
-	isDesktop : function(doc) {
-		return doc && doc.location
-				&& /chrome:\/\/desktop\/content\/desktop.html(\?.*)?/.test(doc.location.href);
-	},
+    isDesktop : function(doc) {
+        return doc && doc.location
+                && /chrome:\/\/desktop\/content\/desktop.html(\?.*)?/.test(doc.location.href);
+    },
 
-	reloadPage : function(doc) {
-		doc.reload(false);
-	},
+    reloadPage : function(doc) {
+        doc.reload(false);
+    },
 
-	forEachDesktopBrowser : function(onPage) {
-		var gBrowser = Utils.getBrowser();
-		for (var i = 0; i < gBrowser.browsers.length; i++) {
-			var br = gBrowser.browsers[i];
-			if (Desktop.isDesktop(br.contentDocument))
-				onPage(br);
-		}
-	},
+    forEachDesktopBrowser : function(onPage) {
+        var gBrowser = Utils.getBrowser();
+        for (var i = 0; i < gBrowser.browsers.length; i++) {
+            var br = gBrowser.browsers[i];
+            if (Desktop.isDesktop(br.contentDocument))
+                onPage(br);
+        }
+    },
 
-	openPreferences : function() {
-		if (!Desktop.prefsWindow || Desktop.prefsWindow.closed) {
-			Desktop.prefsWindow = window.openDialog(
-					"chrome://desktop/content/preferences.xul",
-					"desktop-preferences-window",
-					"chrome,toolbar,centerscreen,resizable=yes");
-		} else
-			Desktop.prefsWindow.focus();
-	},
+    openPreferences : function() {
+        if (!Desktop.prefsWindow || Desktop.prefsWindow.closed) {
+            Desktop.prefsWindow = window.openDialog(
+                    "chrome://desktop/content/preferences.xul",
+                    "desktop-preferences-window",
+                    "chrome,toolbar,centerscreen,resizable=yes");
+        } else
+            Desktop.prefsWindow.focus();
+    },
 
-	isBackgroundImageSpecified : function() {
-		var bg = File.getDataDirectory();
-		bg.append("background");
-		return bg.exists();
-	},
+    isBackgroundImageSpecified : function() {
+        var bg = File.getDataDirectory();
+        bg.append("background");
+        return bg.exists();
+    },
     
     translate : function(key) {
         if (!Desktop.bundle) {
--- defaults/preferences/desktop.js
+++ defaults/preferences/desktop.js
@@ -1,4 +1,5 @@
 pref("extensions.desktop.thumbnail.width", 256);
 pref("extensions.desktop.thumbnail.height", 192);
 pref("extensions.desktop.gridInterval", 32);
+pref("extensions.desktop.backgroundStyle", 1);
 pref("extensions.desktop.focus", "");

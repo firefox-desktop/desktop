3b88f9157a86ce8087fcc7dd5cce468834c4f728
Roman Timushev <rtimush@gmail.com>
2011-03-22 18:47:35 +0000

    Exclude dependencies.txt from XPI
    git-svn-id: http://svn.assembla.com/svn/firefox-desktop/trunk@56 fbf1ad31-3caa-4afa-b5b4-094426959cba

--- makexpi.sh
+++ makexpi.sh
@@ -5,5 +5,5 @@ echo "XPI: " $XPI
 rm $XPI
 mv install.rdf install.rdf.original
 sed -e 's/\(<em:version>.*\)0trunk\(.*<\/em:version>\)/\1'$REVISION'\2/' < install.rdf.original > install.rdf
-zip -r -9 $XPI chrome* defaults install.rdf -x '*.svn/*'
+zip -r -9 $XPI chrome* defaults install.rdf -x '*.svn/*' '*.txt'
 mv install.rdf.original install.rdf

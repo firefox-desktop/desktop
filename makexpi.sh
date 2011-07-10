5d5b8cbbf4244c2954c2e6f50e2855f88b860c71
Roman Timushev <rtimush@gmail.com>
2011-07-10 17:23:01 +0400

    Better release verion handling

--- makexpi.sh
+++ makexpi.sh
@@ -4,6 +4,6 @@ echo "Revision: " $REVISION
 echo "XPI: " $XPI
 rm $XPI
 mv install.rdf install.rdf.original
-sed -e 's/\(<em:version>.*\)0trunk\(.*<\/em:version>\)/\1'$REVISION'\2/' < install.rdf.original > install.rdf
+sed -e 's/\(<em:version>\)[0.]*\(<\/em:version>\)/\1'$REVISION'\2/' < install.rdf.original > install.rdf
 zip -r -9 $XPI chrome* defaults install.rdf -x '*.svn/*' '*.txt'
 mv install.rdf.original install.rdf
